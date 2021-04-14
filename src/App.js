80602d9f1ad8bda7f606b3532d3efdf4b4263032
Cesar Augusto Mosquera Palacios <cmosquerap@loggro.com>
2021-04-13 21:00:26 -0500

    Primera Versión del Proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Table from './components/Table';
 
 function App() {
+
+  const titulo = "Peliculas IUDigital";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <NavBar titulo= {titulo} />
+      
+      <Table />
+
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import react, {useState, useEffect} from 'react';
+
+export default function Table(){
+
+    const baseUrl = "http://localhost:8082/api/";
+
+    const [genders, steGenders] = useState({});
+
+    useEffect(()=>{
+        getGeneros();        
+    }, []);
+
+    const getGeneros = () => {
+        axios.get(`${baseUrl}gender`)
+        .then(response => {
+            steGenders(response.data)
+            console.log(response.data)
+        })
+        .catch(e =>{
+            console.log("Hubo un error" + e);
+        })
+    }
+
+    return (  
+        < >
+      <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Genero ID</th>
+                    <th scope="col">Genero</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    genders.map(item => {
+                        <tr key={item.id} >
+                            <th scope="row">{item.id}</th>
+                            <td>Mark</td>
+                        </tr>
+                    })
+                }
+            </tbody>
+      </table>
+        </>
+    );
+}
